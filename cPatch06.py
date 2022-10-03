0bd2df7056fa58a3a8381bdeab9aacb45ee10f09
Lee-Song-Ha <songha1744@swu.ac.kr>
2022-10-03 15:18:20 +0900

    송 - 파일 정리

--- cPatch06.py
+++ cPatch06.py
@@ -4,10 +4,18 @@ command = "echo $PATH"
 stream = os.popen(command)
 result = stream.read()
 
+
 try:
     resultList = list(result)
     if "." in resultList:
         print("PATH START")
+
+        resultList.remove(".")
+        path = ''.join(resultList)
+
+        pathCommand = ("PATH=%s" %path)
+        os.system(pathCommand)
+        print("PATH END")
 
     else:
         print("No Problem")
