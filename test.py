fbada090bce619d91ae0d64ed9e31ffc21c2d59f
Lee-Song-Ha <songha1744@swu.ac.kr>
2022-10-03 13:51:25 +0900

    os.popen 명령어 사용

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-import os
-import subprocess
+# /etc/passwd 파일의 퍼미션과 소유자를 확인
+# /etc/passwd 파일의 소유자 및 권한 변경 (소유자 root, 권한 644)
 
-command = "ls -l"
-result = subprocess.run(command.split(' '), stdout=subprocess.PIPE, text=True)
+import os
+
+command = "echo $PATH"
+stream = os.popen(command)
+result = stream.read()
 
 try:
-    print(result.stdout)
+    print(result)
     print("success")
 
 except:
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import os
-import subprocess
-
-command = "ls -l"
-result = subprocess.run(command.split(' '), stdout=subprocess.PIPE, text=True)
-print(result.stdout)
